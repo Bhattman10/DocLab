d58dd841b48b84cb1a2c961cd50c213f60af5915
Ethan Bhatt <ethanbhatt2010@gmail.com>
2025-02-06 00:06:40 -0500

    added more comments to JS file

--- DocLabs-Test/test.js
+++ DocLabs-Test/test.js
@@ -1,8 +1,15 @@
+// JS Fetch API Docs -> https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+// Our deployed version will utilize Google's version of Fetch.
+
 verify();
 send();
 
+// Verify if Flask API is up and running
 async function verify() {
+
+    // This is the API url
     const url = "http://127.0.0.1:5000/";
+
     try {
         const response = await fetch(url);
         if (!response.ok) {
@@ -13,12 +20,16 @@ async function verify() {
     }
 }
 
+// Send data to the server and recieve the execution result
 async function send() {
 
+    // Have some headers included here... do we need them?
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
+
     const response = await fetch("http://127.0.0.1:5000/", {
         method: "POST",
+        // Change the contents of the JSON to experiment!
         body: JSON.stringify(
             {
                 session_id: '12345',
@@ -29,6 +40,8 @@ async function send() {
         ),
         headers: myHeaders,
     })
+        // These commands wait for the JSON response from the server.
+        // Is this proper syntax for the POST method? ChatGPT says no...
         .then(response => response.json())
         .then(data => { console.log(data.result); });
 }
